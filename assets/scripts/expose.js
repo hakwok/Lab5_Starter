341a7f3bfb4e05a0ffc380cf0a677d960b781d71
Hayden Kwok <hayden.kwok@gmail.com>
2024-05-02 03:35:35 -0700

    finished expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,67 @@
-// expose.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSelect = document.getElementById('horn-select');
+  const imageElement = document.querySelector('img[alt="No image selected"]');
+  const audioElement = document.querySelector('audio');
+  const volumeSlider = document.getElementById('volume');
+  const volumeIcon = document.querySelector('#volume-controls img');
+  const playButton = document.querySelector('button');
+  const jsConfetti = new JSConfetti();
+
+  // Mapping horn types to their respective images and audio files
+  const hornData = {
+    'air-horn': {
+      img: 'assets/images/air-horn.svg',
+      audio: 'assets/audio/air-horn.mp3'
+    },
+    'car-horn': {
+      img: 'assets/images/car-horn.svg',
+      audio: 'assets/audio/car-horn.mp3'
+    },
+    'party-horn': {
+      img: 'assets/images/party-horn.svg',
+      audio: 'assets/audio/party-horn.mp3'
+    }
+  };
+
+  hornSelect.addEventListener('change', function() {
+    const selectedHorn = hornSelect.value;
+    if(hornData[selectedHorn]){
+      imageElement.src = hornData[selectedHorn].img;
+      imageElement.alt = selectedHorn.replace('-', ' ') + ' image';
+      audioElement.src = hornData[selectedHorn].audio;
+    }
+  });
+
+
+  volumeSlider.addEventListener('input', updateVolume);
+
+  function updateVolume() {
+    const volumeValue = parseInt(volumeSlider.value, 10);
+    let volumeImagePath;
+
+    if (volumeValue === 0) {
+      volumeImagePath = 'assets/icons/volume-level-0.svg';
+    } 
+    else if (volumeValue < 33) {
+      volumeImagePath = 'assets/icons/volume-level-1.svg';
+    } 
+    else if (volumeValue < 67) {
+      volumeImagePath = 'assets/icons/volume-level-2.svg';
+    } 
+    else {
+      volumeImagePath = 'assets/icons/volume-level-3.svg';
+    }
+
+    volumeIcon.src = volumeImagePath;
+    audioElement.volume = volumeValue / 100;
+  }
+
+  playButton.addEventListener('click', function() {
+    audioElement.play();
+    if (hornSelect.value === 'party-horn') {
+      jsConfetti.addConfetti();
+    }
+  });
 }
